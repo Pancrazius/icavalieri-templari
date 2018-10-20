c19734d1dea5165ee2160b81c8dc00e7d58a1bf5
Castellano <grazianocas90@gmail.com>
2018-10-20 10:53:23 +0200

    Create base file

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-ciao
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>I Cavalieri Templari</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="intestazione">
+    <img src="img/Intestazione.png"/>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+    </div>
+    <div class="col-sm-4">
+    </div>
+    <div class="col-sm-4">
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
