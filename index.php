2b0aacd2620f6b3f0c99a6bf9a5328f3e76d622a
Castellano <grazianocas90@gmail.com>
2018-10-20 12:36:38 +0200

    Create base file

--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>I Cavalieri Templari</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+    <head>
+        <title>I Cavalieri Templari</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/index.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
 
-<body>
+    <body>
 
-<div class="intestazione">
-    <img src="img/Intestazione.png"/>
-</div>
-  
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-    </div>
-    <div class="col-sm-4">
-    </div>
-    <div class="col-sm-4">
-    </div>
-  </div>
-</div>
+        <div class="intestazione">
+            <img class="logoIntestazione" src="img/intestazione.png"/>
+        </div>
 
-</body>
+        <div class="indexBody">
+            <div class="row">
+                <div class="col-sm-4 ">
+                    <div class="indexFirstCol">
+                        <a  href="https://icavalieri-templari.forumfree.it/?act=idx">
+                            <img class ="indexIcon" src="img/Forum.png"/>
+                            <h3 class="indexFirstCol">FORUM</h3>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4 indexSecondCol">
+                    <img class="indexImageCenter" src="img/illidan.gif" />
+                </div>
+                <div class="col-sm-4">
+                    <div class="indexFirstCol">
+                        <a href="https://www.icavalieri-templari.it/dkp/index.php/?s=">
+                            <img class ="indexIcon" src="img/planner.png"/>
+                            <h3 class="indexFirstCol">PLANNER</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
 </html>
